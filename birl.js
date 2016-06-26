76590ac0da85f88933cf0657efd3cce067704e2c
Isac Fadoni <isacfadoni@gmail.com>
2016-06-26 20:21:42 -0300

    mend
    Audio - Added the Bamban's werid sounds.

--- birl.js
+++ birl.js
@@ -1,31 +1,34 @@
-var path = require("path");
-var pressed = {};
+var path = require("path")
+  , pressed = {}
+  , key_down = null
+  , editorView = null
+;
 
-function handleKeyDown(event) {
+function handleKeyDown (event) {
 	if (!pressed[event.keyCode]) {
 		pressed[event.keyCode] = true;
 
-		var key_down = new Audio(path.join(__dirname, "audio/key_down.mp3"));
-		key_down.play();
-
 		if (event.keyCode === 13) {
-			var key_down = new Audio(path.join(__dirname, "audio/return.mp3"));
+			key_down = new Audio(path.join(__dirname, "audio/porra.mp3"));
 			key_down.play();
-		}
+		} else if (event.keyCode === 8) {
+      key_down = new Audio(path.join(__dirname, "audio/eitaporra.mp3"));
+      key_down.play();
+    } else {
+      key_down = new Audio(path.join(__dirname, "audio/birl.mp3"));
+      key_down.play();
+    }
 	}
 }
 
-function handleKeyUp(event) {
-		pressed[event.keyCode] = false;
-
-		var key_up = new Audio(path.join(__dirname, "audio/key_up.mp3"));
-		key_up.play();
+function handleKeyUp (event) {
+	pressed[event.keyCode] = false;
 }
 
 module.exports = {
-	activate: function(state) {
-		atom.workspace.observeTextEditors(function(editor) {
-			var editorView = atom.views.getView(editor);
+	activate: function (state) {
+		atom.workspace.observeTextEditors(function (editor) {
+      editorView = atom.views.getView(editor);
 
 			editorView.addEventListener('keydown', handleKeyDown);
 			editorView.addEventListener('keyup', handleKeyUp);
@@ -33,8 +36,8 @@ module.exports = {
 	},
 
 	deactivate: function () {
-		atom.workspace.observeTextEditors(function(editor) {
-			var editorView = atom.views.getView(editor);
+		atom.workspace.observeTextEditors(function (editor) {
+      editorView = atom.views.getView(editor);
 
 			editorView.removeEventListener('keydown', handleKeyDown);
 			editorView.removeEventListener('keyup', handleKeyUp);
